c3d97fc5251c0d418ba74000d9edd710fbc0f542
Aviv Klasquin Komissar <avivklas@users.noreply.github.com>
2017-11-23 16:53:32 +0200

    a fix for 32bit systems

--- cmd/webm-player/audio.go
+++ cmd/webm-player/audio.go
@@ -94,7 +94,7 @@ func paCallback(channels int, syncC <-chan time.Duration, aOut <-chan Samples) p
 			return statusComplete
 		}
 
-		out := (*(*[1 << 32]float32)(output))[:int32(sampleCount)*int32(channels)]
+		out := (*(*[1 << 16]float32)(output))[:int32(sampleCount)*int32(channels)]
 		if len(samples.DataInterleaved) > 0 {
 			copy(out, samples.DataInterleaved[:int32(sampleCount)*int32(channels)])
 			return statusContinue
